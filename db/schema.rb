b9c1ead7119b02cbb23d55561c698ed2d1e97ab2
Maxim <karamba92@mail.ru>
2017-07-24 20:35:48 +0600

    #7 добавил модель Роль, добавил роли в сиды, приделал роль пользователю, добавил выпадающий список Ролей в список

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724093701) do
+ActiveRecord::Schema.define(version: 20170724120258) do
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -34,8 +40,10 @@ ActiveRecord::Schema.define(version: 20170724093701) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer "role_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
 end
