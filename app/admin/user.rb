8cb98766e5f9d38404b972dc21daaca3b1e219a8
Maxim <karamba92@mail.ru>
2017-07-26 14:50:13 +0600

    #7 вывел имена ролей в админке

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,29 +1,37 @@
 ActiveAdmin.register User do
 
-  permit_params :email, :password, :password_confirmation, :role_id
+  permit_params :name, :surname, :description, :phone, :social_network, :email, :password,
+                :password_confirmation, role: [:id, :name, ]
 
   index do
     selectable_column
     id_column
-    column :email
     column :name
-    column :role_id
+    column :surname
+    column :description
+    column :phone
+    column :social_network
+    column :email
+    column :role
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
     actions
   end
 
-
   form do |f|
     f.inputs do
+      f.input :name
+      f.input :surname
+      f.input :description
+      f.input :phone
+      f.input :social_network
       f.input :email
-      f.input :password
-      f.input :password_confirmation
       f.input :role_id, :label => 'Профессия', :as => :select,
               :collection => Role.all.map{|role| ["#{role.name}", role.id]}
+      f.input :password
+      f.input :password_confirmation
     end
     f.actions
   end
-
 end
