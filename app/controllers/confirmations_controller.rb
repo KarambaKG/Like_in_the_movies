40d098482f186df34e55df69e0037134ea4fe0c9
Maxim <karamba92@mail.ru>
2017-07-27 14:25:14 +0600

    #7 при отправке заявки, пользователю приходит письмо для подтверждения почты, после подтверждения почты, письмо приходит админу, о том что появилась новая заявка

---
+++ app/controllers/confirmations_controller.rb
@@ -0,0 +1,16 @@
+class ConfirmationsController < Devise::ConfirmationsController
+  protected
+  def after_confirmation_path_for(resource_name, resource)
+    if signed_in?(resource_name)
+      AdminUser.all.each do |admin|
+        ToAdminMailer.reg_alert(admin).deliver_now
+      end
+      signed_in_root_path(resource)
+    else
+      AdminUser.all.each do |admin|
+        ToAdminMailer.reg_alert(admin).deliver_now
+      end
+      new_session_path(resource_name)
+    end
+  end
+end
---
+++ app/mailers/to_admin_mailer.rb
@@ -0,0 +1,16 @@
+class ToAdminMailer < ApplicationMailer
+  default from: 'karambavkino@gmail.com'
+
+  def reg_alert(user)
+    mail(to: user.email, subject: 'Новый пользователь зарегистрирован')
+  end
+
+  def confirm_alert(user)
+    mail(to: user.email, subject: 'Подтверждение')
+  end
+
+  def reject_alert(user)
+    mail(to: user.email, subject: 'Отказ')
+  end
+
+end
