9df8563e76661a9d418600fb89106a486852cde8
Maxim <karamba92@mail.ru>
2017-08-11 14:30:36 +0600

    #10 добавил страничку пользователя, вывел его награды

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -12,6 +12,10 @@ class MainsController < ApplicationController
     @all_command_participants = User.where(command: Command.find_by_name(@about_command.name))
   end
 
+  def about_user
+    @user = User.find(params[:id])
+  end
+
   def movies
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
 
   get '/commands', to: 'mains#commands', as: 'commands'
   get '/about_command/:id', to: 'mains#about_command', as: 'about_command'
+  get '/about_user/:id', to: 'mains#about_user', as: 'about_user'
   get '/movies', to: 'mains#movies', as: 'movies'
   get '/participants', to: 'mains#participants', as: 'participants'
   get '/contacts', to: 'mains#contacts', as: 'contacts'
   get '/help_project', to: 'mains#help_project', as: 'help_project'
 
 end
-
