d82e75c47c97ce510be3cf9df460f4c4a22b6e39
Maxim <karamba92@mail.ru>
2017-08-11 15:50:55 +0600

    #11 поправил ошибку если пользователь был без команды, перевёл все роли на русский

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # Создание ролей
-%w(operator producer sound_director lighter editor actor pr_manager makeup composer screenwriter photographer
-sponsor).each do |role|
+%w(оператор продюсер звукорежиссёр осветитель редактор актёр pr-менеджер гримёр композиток
+ сценарист фотограф спонсор).each do |role|
   Role.find_or_create_by({name: role})
 end
 
