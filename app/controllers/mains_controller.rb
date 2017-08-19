7ce7d2e9db29152e3e8b23f669e9394890e7221c
Maxim <karamba92@mail.ru>
2017-08-19 18:45:10 +0600

    #12 добавил сортировку участников по ролям и командам, поправил вывод если нет картинки у пользователя, активировал ссылку на переход информации о пользователе

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -28,8 +28,111 @@ class MainsController < ApplicationController
   def help_project
   end
 
-  def results
 
+  #Экшены поиска участников
+
+  def in_commands
+    @in_commands = User.where(command_id: !nil)
+  end
+
+  def no_commands
+    @no_commands = User.where(command_id: nil)
+  end
+
+  def in_commands_operators
+    @in_commands_operators = User.where(role: Role.find_by_name('оператор'), command_id: !nil)
+  end
+
+  def no_commands_operators
+    @no_commands_operators = User.where(role: Role.find_by_name('оператор'), command_id: nil)
+  end
+
+  def in_commands_producers
+    @in_commands_producers = User.where(role: Role.find_by_name('продюсер'), command_id: !nil)
+  end
+
+  def no_commands_producers
+    @no_commands_producers = User.where(role: Role.find_by_name('продюсер'), command_id: nil)
+  end
+
+  def in_commands_sound_directors
+    @in_commands_sound_directors = User.where(role: Role.find_by_name('звукорежиссёр'), command_id: !nil)
+  end
+
+  def no_commands_sound_directors
+    @no_commands_sound_directors = User.where(role: Role.find_by_name('звукорежиссёр'), command_id: nil)
+  end
+
+  def in_commands_lighters
+    @in_commands_lighters = User.where(role: Role.find_by_name('осветитель'), command_id: !nil)
+  end
+
+  def no_commands_lighters
+    @no_commands_lighters = User.where(role: Role.find_by_name('осветитель'), command_id: nil)
+  end
+
+  def in_commands_editors
+    @in_commands_editors = User.where(role: Role.find_by_name('редактор'), command_id: !nil)
+  end
+
+  def no_commands_editors
+    @no_commands_editors = User.where(role: Role.find_by_name('редактор'), command_id: nil)
+  end
+
+  def in_commands_actors
+    @in_commands_actors = User.where(role: Role.find_by_name('актёр'), command_id: !nil)
+  end
+
+  def no_commands_actors
+    @no_commands_actors = User.where(role: Role.find_by_name('актёр'), command_id: nil)
+  end
+
+  def in_commands_pr_managers
+    @in_commands_pr_managers = User.where(role: Role.find_by_name('pr-менеджер'), command_id: !nil)
+  end
+
+  def no_commands_pr_managers
+    @no_commands_pr_managers = User.where(role: Role.find_by_name('pr-менеджер'), command_id: nil)
+  end
+
+  def in_commands_makeups
+    @in_commands_makeups = User.where(role: Role.find_by_name('гримёр'), command_id: !nil)
+  end
+
+  def no_commands_makeups
+    @no_commands_makeups = User.where(role: Role.find_by_name('гримёр'), command_id: nil)
+  end
+
+  def in_commands_composers
+    @in_commands_composers = User.where(role: Role.find_by_name('композитор'), command_id: !nil)
+  end
+
+  def no_commands_composers
+    @no_commands_composers = User.where(role: Role.find_by_name('композитор'), command_id: nil)
+  end
+
+  def in_commands_writers
+    @in_commands_writers = User.where(role: Role.find_by_name('сценарист'), command_id: !nil)
+  end
+
+  def no_commands_writers
+    @no_commands_writers = User.where(role: Role.find_by_name('сценарист'), command_id: nil)
+  end
+
+  def in_commands_photographers
+    @in_commands_photographers = User.where(role: Role.find_by_name('фотограф'), command_id: !nil)
+  end
+
+  def no_commands_photographers
+    @no_commands_photographers = User.where(role: Role.find_by_name('фотограф'), command_id: nil)
+  end
+
+  def in_commands_sponsors
+    @in_commands_sponsors = User.where(role: Role.find_by_name('спонсор'), command_id: !nil)
+  end
+
+  def no_commands_sponsors
+    @no_commands_sponsors = User.where(role: Role.find_by_name('спонсор'), command_id: nil)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
-                    default_url: "/images/missing/missing.png"
+                    default_url: "missing/missing.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   def active_for_authentication?
